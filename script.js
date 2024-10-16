df25df06df488e830d22f575c8bdb6708cf90ae6
ezcode <147483544+ezcodeweb@users.noreply.github.com>
2024-10-16 14:38:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function onloadCallback() {
     var recaptcha1 = grecaptcha.render('recaptcha-container', {
-        'sitekey' : '6LfF6GIqAAAAABxBAiap2rHvo4wUvQ2mt2-OLhs7'
+        'sitekey' : '6LfF6GIqAAAAAAvJh8zs9zl2hXJhIkmho7s0ObMe'
     });
 }
 
 document.getElementById('submit-button').addEventListener('click', function() {
     grecaptcha.ready(function() {
-        grecaptcha.execute('6LfF6GIqAAAAABxBAiap2rHvo4wUvQ2mt2-OLhs7', {action: 'submit'}).then(function(token) {
+        grecaptcha.execute('6LfF6GIqAAAAAAvJh8zs9zl2hXJhIkmho7s0ObMe', {action: 'submit'}).then(function(token) {
             // Send the token to your server for verification
             console.log('reCAPTCHA token:', token);
 
